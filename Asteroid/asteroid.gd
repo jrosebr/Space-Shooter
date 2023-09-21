1ef62524e369220c982ce545eb43d5b56a3371e4
jrosebr <jrosebr@iu.edu>
2023-09-21 00:13:21 -0400

    Did Project 2 Part 4

--- Asteroid/asteroid.gd
+++ Asteroid/asteroid.gd
@@ -9,12 +9,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
 	move_and_slide()
-	position.x = wrapf(position.x, 0, 1152)
-	position.y = wrapf(position.y, 0, 648)
+	position.x = wrapf(position.x, 0, Global.VP.x)
+	position.y = wrapf(position.y, 0, Global.VP.y)
 	
 	print(velocity.length())
 
 func damage(d):
 	health -= d
 	if health <= 0:
+		Global.update_score(100)
 		queue_free()
